a8cef976db3a5e48381ab29ab732eb951ef0d448
Danilo Lemes <daniloleemes@hotmail.com>
2024-01-01 14:28:01 +0000

    part 2

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ haystack_img = cv.imread('images/screen.png', cv.IMREAD_UNCHANGED)
 needle_img = cv.imread('images/toad.png', cv.IMREAD_UNCHANGED)
 
 result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
+threshold = 0.80
+locations = np.where(result >= threshold)
+locations = list(zip(*locations[::-1]))
 
-# get the best match position
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-
-threshold = 0.8
-if max_val > threshold:
-    print('found')
-    
+if locations:
     needle_h = needle_img.shape[0]
     needle_w = needle_img.shape[1]
+    line_color = (0, 255, 0)
+    line_type = cv.LINE_4
 
-    top_left = max_loc
-    bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+    for loc in locations:
+        top_left = loc
+        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+        cv.rectangle(haystack_img, top_left, bottom_right, line_color, 1, line_type)
 
-    cv.rectangle(haystack_img, top_left, bottom_right, color=(0,255,0), thickness=2, lineType=cv.LINE_4)
-    cv.imshow('Result', haystack_img)
+    cv.imshow("Result", haystack_img)
     cv.waitKey()
 else:
-    print('not found')
+    print("no results")
